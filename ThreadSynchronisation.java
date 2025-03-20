6c1b3cd7ef3a230953e3ca530c759297891e6d1a
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-21 00:10:42 +0530

    thread synchronisation

---
+++ ThreadSynchronisation.java
@@ -0,0 +1,13 @@
+public class ThreadSynchronisation {
+    /*
+     * Thread synchronization prevents multiple threads from accessing the same resource at the same time, avoiding race conditions and inconsistent data.
+     * 
+     * Why Do We Need Synchronization?
+     *      Without synchronization:
+     *          * Threads interfere with each other.
+     *          * Data becomes inconsistent.
+     *          * Unpredictable race conditions occur.
+     *      Synchronization ensures that only one thread can access a shared resource at a time.
+     *      
+     */
+}
