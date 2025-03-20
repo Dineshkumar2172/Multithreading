2fb8c2b67070de59afb4dba20613a5adcb82bfe1
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-20 23:32:40 +0530

    process & thread

---
+++ ProcessAndThreads.java
@@ -0,0 +1,7 @@
+public class ProcessAndThreads {
+    /*
+     * 1. Process is an instance of program execution. When you enter an application, it's a process. The OS assigns it's own stack & heap memory area.
+     * 
+     * 2. Thread is a lightweight process. It is a unit of execution within a given program. A single process may contains multiple threads. Each thread in the memory shares the memory and resources.
+     */
+}
